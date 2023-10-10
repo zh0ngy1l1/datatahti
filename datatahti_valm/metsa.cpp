6089179331f5fb168a3033e86831da84efa64de0
zh0ngy1l1 <zhongyi.david.li@gmail.com>
2023-10-10 09:52:38 +0300

    finished metsa.cpp. I'm too smart.

--- datatahti_valm/metsa.cpp
+++ datatahti_valm/metsa.cpp
@@ -5,8 +5,8 @@ using namespace std;
 #define ll long long
 
 int main() {
-    //ios::sync_with_stdio(0);
-    //cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     int n;
     int m;
     ll q;
@@ -33,40 +33,20 @@ int main() {
         cin >> x1;
         cin >> y2;
         cin >> x2;
+        
+        int sum = 0;
 
-        for (int i = y1 -1 ; i < y2 ; i ++) {
-            cout << (rows[i][x2 - 1] - rows[i][x1 - 1] )
+        if (x1 == 1) {
+            for (int i = y1 - 1 ; i < y2 ; i ++) {
+                sum += (rows[i][x2 - 1]);
+            }
+        } else {
+            for (int i = y1 - 1 ; i < y2 ; i ++) {
+                sum += (rows[i][x2 - 1] - rows[i][x1 - 2] );
+            }
         }
-
+        cout << sum << endl;
 
     }
 
 }
-
-// g++ -o <name-you-want-to-give> source.cpp
-// n, m, q = tuple([int(i) for i in input().split()])
-// rows = []
-// for _ in range(n):
-//     line = input()
-//     rows.append([i for i in range(m) if line[i] == "*"])
-// output = ""
-
-// for _ in range(q):
-//     result = 0
-//     a = 0
-//     b = 0
-//     rect = [int(i) - 1 for i in input().split()]
-//     for row in rows[rect[0]:rect[2] + 1]:
-//         for i in row:
-//             if i >= rect[1]:
-//                 break
-//             a += 1
-//         for j in row[::-1]:
-//             if j <= rect[2]:
-//                 break
-//             b += 1
-//         result = result + len(row) - a - b
-//     output = output + str(result) + "\n"
- 
-// #print(output[:-1])
-// print("success")
