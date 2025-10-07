e1f1e11c724958c132a27be1749bc88319461094
zh0ngy1l1 <zhongyi.david.li@gmail.com>
2025-10-07 11:32:06 +0300

    removed cpp

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # datatahti
-learnings toward datatähti
+This used to be 'learning toward datatähti.'
+Now it's my go-to repo for random shit.
--- chapter3/p5.py
+++
@@ -1,9 +0,0 @@
-n=int(input())
-s=[*map(int,input().split()),n+1]
-m=x=n
-for i in range(n):
-    if m < s.index(n-i):
-        break
-    m = s.index(n-i)
-    x-=1
-print(x)
